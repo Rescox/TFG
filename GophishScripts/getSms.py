52545b1085871d5277932a61ce1a657511e9495d
Raúl <38223132+Rescox@users.noreply.github.com>
2021-05-04 13:14:16 +0200

    Update getSms.py

--- GophishScripts/getSms.py
+++ GophishScripts/getSms.py
@@ -4,14 +4,14 @@ import urllib.request, json
 import requests
 import time
 
-account_sid = "AC4c1c7241ea7179ed6f998321ea9347c4" #os.environ['TWILIO_ACCOUNT_SID']
-auth_token = "f8cd10fa47a9c0aa565fec3d8629015a" #os.environ['TWILIO_AUTH_TOKEN']
+account_sid = "Cuenta de Twilio" #os.environ['TWILIO_ACCOUNT_SID']
+auth_token = "Token de cuenta de Twilio" #os.environ['TWILIO_AUTH_TOKEN']
 client = Client(account_sid, auth_token)
 print("Iniciando listener de SMS")
 
 
 while(True):
-    with urllib.request.urlopen("http://192.168.1.58:4000/sms") as url:
+    with urllib.request.urlopen(UrlBaseDeDatosSMS) as url:
         data = json.loads(url.read().decode())
         for campaign in data:
             if campaign['state'] == "Created":
@@ -20,16 +20,16 @@ while(True):
                     sCadena = campaign['body'][0]
                     sCadena = sCadena.replace("{.FirstName}", group["First Name"])
                     sCadena = sCadena.replace("{.LastName}", group["Last Name"])
-                    sCadena = sCadena.replace("{.URL}", "192.168.1.58:8080" + "?nSms=644186074&id=" +campaign['_id'])
+                    sCadena = sCadena.replace("{.URL}", URLTwilioScript + "?nSms=644186074&id=" +campaign['_id'])
                     message = client.messages.create(
                             body= sCadena,
-                            from_='+12512368663',
+                            from_=Numero proporcionado por Twilio,
                             to= '+34' + group['Telephone']
                     )
                     print(sCadena)
                     
                     
-                r = requests.put('http://192.168.1.58:4000/sms/'+campaign['_id'], json = campaign)
+                r = requests.put(UrlBaseDeDatosSMS + '+campaign['_id'], json = campaign)
 
     time.sleep(5)
 
