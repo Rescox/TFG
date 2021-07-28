f9ade0106b5161684ef7df2a25a748bb57d515d4
Raúl <38223132+Rescox@users.noreply.github.com>
2021-07-28 12:28:47 +0200

    Update db.js

--- src/backend/db.js
+++ src/backend/db.js
@@ -1,7 +1,7 @@
 const { mongo } = require('mongoose')
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://rescoTFG:rescoTFG1234@tfg.1etzg.mongodb.net/TFGEmails?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology:true},
+mongoose.connect('', {useNewUrlParser: true, useUnifiedTopology:true},
 err => { 
     if(!err)
         console.log('Mongodb working')
